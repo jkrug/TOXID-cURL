ca907ca9ffbe0c0814b0c0a8c02ca90d7210f2b4
Mazvydas Skuodas <wanis1@gmail.com>
2011-08-08 13:34:46 +0300

    oxviewconfig module for getting toxid object. Needs in config.inc.php: $this->sTplVariable = 'toxid_curl_cmp';
    fixed issue #518: http://tracker.marmalade.de/view.php?id=518

---
+++ eshop/modules/toxid_curl/core/toxid_curl_oxviewconfig.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ *    This file is part of TOXID Module for OXID eShop CE/PE/EE.
+ *
+ *    TOXID is free software: you can redistribute it and/or modify
+ *    it under the terms of the MIT License.
+ *
+ *
+ * @link      http://toxid.org
+ * @link      http://marmalade.de
+ * @package   core
+ * @copyright (C) marmalade.de 2011
+ */
+
+class toxid_curl_oxviewconfig extends toxid_curl_oxviewconfig_parent
+{
+    /**
+     * calls _injectTplVariable and returns parent constructor result
+     */
+    public function __construct()
+    {
+        $this->_injectTplVariable();
+        return parent::__construct();
+    }
+
+    /**
+     * if config variable sTplVariable is set,
+     * sets template variable with getToxid() result
+     * @return void
+     */
+    protected function _injectTplVariable()
+    {
+        $oConfig = oxConfig::getInstance();
+        $sTplVariableName = $oConfig->getConfigParam('sTplVariable');
+        if ($sTplVariableName) {
+            $oConfig->getActiveView()->addTplParam($sTplVariableName, $this->getToxid());
+        }
+    }
+
+    /**
+     * returns instance of toxidCurl
+     * @return toxidCurl
+     */
+    public function getToxid()
+    {
+        $oToxid = toxidCurl::getInstance();
+        $oToxid->loadCmsPage();
+        return $oToxid;
+    }
+}
