2a50fb15988ca88cf6542192ce2751847764ab7a
Mazvydas Skuodas <wanis1@gmail.com>
2011-10-28 18:08:54 +0300

    added multilang support for rewriting scripts. rewrite called only for snippet, not hole XML. + changed tabs to spaces

--- eshop/modules/toxid_curl/core/toxidcurl.php
+++ eshop/modules/toxid_curl/core/toxidcurl.php
@@ -26,45 +26,51 @@ class toxidCurl extends oxSuperCfg
      * @var toxidcurl instance
      */
     private static $_instance = null;
-		
+
     /**
      * array of content snippets
      *
      * @var array
      */
     public static $aToxidSnippets = null;
-	
-	/**
+
+    /**
      * object
      *
      * @var object
      */
     private $_oSxToxid = null;
-	
+
     /**
      * string with XML-content
      *
      * @var string
      */
     private $_sPageContent = null;
-	
+
     /**
      * stores URL from which typo3 content is loaded.
-     * @var string
+     * @var array
      */
-    protected $_sSourceUrlByLang = null;
+    protected $_aSourceUrlByLang = null;
 
     /**
      * Stores URI to which typo3 urls will be converted, so it will be loaded in oxid
-     * @var string
+     * @var array
      */
-    protected $_sRewriteStartUrl = null;
+    protected $_aRewriteStartUrl = null;
+
+    /**
+     * array of string with language specific toxidUrlParam
+     * @var array
+     */
+    protected $_aToxidLangUrlParam = null;
 
     /**
      * stores search url by active language
-     * @var string
+     * @var array
      */
-    protected $_sSearchUrl = null;
+    protected $_aSearchUrl = null;
 
     /**
      * stores search results
@@ -86,87 +92,110 @@ class toxidCurl extends oxSuperCfg
         }
         return self::$_instance;
     }
-	
-	/**
+
+    /**
      * content will be loaded on first usage needed
      * @deprecated
      */
     public function loadCmsPage() {}
-	
-	/**
-     * should later generate Cache
+
+    /**
+     * returns SimpleXMLElement object from Typo3 xml
+     * @param bool $blReset
+     * @return SimpleXMLElement
      */
-    protected function _generateCache($blReset = false)
-	{
-		if ($this->_oSxToxid !== null && !$blReset) {
+    protected function _getXmlObject($blReset = false)
+    {
+        if ($this->_oSxToxid !== null && !$blReset) {
             return $this->_oSxToxid;
         }
-        $this->_readURL();
-		$this->_sPageContent = $this->_rewriteUrls($this->_sPageContent);
-		$this->_oSxToxid = simplexml_load_string($this->_sPageContent);
+        $this->_oSxToxid = simplexml_load_string($this->_getXmlFromTypo3($blReset));
         return $this->_oSxToxid;
 
-	}
-	
-	/**
-     * returns the called snippet
-     */
-    public function getCmsSnippet($snippet=null)
-	{
-        $this->_generateCache();
-		if($snippet == null)
-		{
-			return '<strong style="color:red;">TOXID: Please add part, you want to display!</strong>';
-		}
+    }
 
-        $aXpathSnippets = $this->_oSxToxid->xpath('//'.$snippet.'[1]');
+    /**
+     * returns snippet text from xml object
+     * @param $sSnippet
+     * @return SimpleXMLElement
+     */
+    protected function _getSnippetFromXml($sSnippet)
+    {
+        $oTypo3Xml = $this->_getXmlObject();
+        $aXpathSnippets = $oTypo3Xml->xpath('//'.$sSnippet.'[1]');
         $sText = $aXpathSnippets[0];
+
+        return $sText;
+
+    }
+
+    /**
+     * returns the called snippet
+     * @param string $snippet
+     * @param bool $blMultiLang
+     * @return string
+     */
+    public function getCmsSnippet($snippet=null, $blMultiLang = false)
+    {
+        if($snippet == null) {
+            return '<strong style="color:red;">TOXID: Please add part, you want to display!</strong>';
+        }
+        $sText = $this->_getSnippetFromXml($snippet);
+        $sText = $this->_rewriteUrls($sText, null, $blMultiLang);
+
+
         $sShopId = $this->getConfig()->getActiveShop()->getId();
         $sLangId = oxLang::getInstance()->getBaseLanguage();
         $sText = oxUtilsView::getInstance()->parseThroughSmarty(
             $sText,
             $snippet.'_'.$sShopId.'_'.$sLangId
         );
-		
-		if($this->getConfig()->getConfigParam('iUtfMode') !== 1)
-		{
-			$sText = str_replace('€', '&euro;', $sText);
-			return utf8_decode($sText);
-		} else {
-			return $sText;
-		}
-	}
-	
-	/**
-	 * returns raw string from CMS-page
-	 */
-	protected function _readUrl()
-	{
-		$source = $this->_getToxidLangSource();
-		$page = $this->getConfig()->getConfigParam('sToxidCurlPage');
-		$param = $this->_getToxidLangUrlParam();
-		//$param = $this->getConfig()->getConfigParam('sToxidCurlUrlParam');
 
-		$aPage = $this->_getRemoteContent($source.$page.$param);
-		
-		//$aPage = utf8_decode($aPage);
+        if($this->getConfig()->getConfigParam('iUtfMode') !== 1)
+        {
+            $sText = str_replace('ï¿½', '&euro;', $sText);
+            return utf8_decode($sText);
+        } else {
+            return $sText;
+        }
+    }
 
+    /**
+     * returns raw string from typo3 CMS-page
+     * @param bool $blReset set to true if you want to fetch content again
+     * @return string
+     */
+    protected function _getXmlFromTypo3($blReset = false)
+    {
+        if ($this->_sPageContent !== null && !$blReset) {
+            return $this->_sPageContent;
+        }
+        
+        $source = $this->_getToxidLangSource();
+        $page = $this->getConfig()->getConfigParam('sToxidCurlPage');
+        $param = $this->_getToxidLangUrlParam();
+
+        $aPage = $this->_getRemoteContent($source.$page.$param);
+        
         $this->_sPageContent = $aPage['content'];
-		
-		switch ($aPage['info']['http_code'])
-		{
-			case 500:
-				header ("HTTP/1.1 500 Internal Server Error");
-				header ('Location: '.$this->getConfig()->getShopHomeURL());
-				oxUtils::getInstance()->showMessageAndExit('');
-                break;
-			case 404:
-				header ("HTTP/1.1 404 Not Found");
-				header ('Location: '.$this->getConfig()->getShopHomeURL());
+
+        switch ($aPage['info']['http_code'])
+        {
+            case 500:
+                header ("HTTP/1.1 500 Internal Server Error");
+                header ('Location: '.$this->getConfig()->getShopHomeURL());
                 oxUtils::getInstance()->showMessageAndExit('');
                 break;
-		}
-	}
+            case 404:
+                header ("HTTP/1.1 404 Not Found");
+                header ('Location: '.$this->getConfig()->getShopHomeURL());
+                oxUtils::getInstance()->showMessageAndExit('');
+                break;
+        }
+
+        return $this->_sPageContent;
+
+    }
 
     /**
      * returns array with result of http get. array structure:
@@ -191,91 +220,120 @@ class toxidCurl extends oxSuperCfg
         curl_close($curl_handle);
         return $aResult;
     }
-	
-	/**
-	 * returns string with toxidStartUrl
-	 */
-	public function getToxidStartUrl()
-	{
-		return preg_replace('#index.php\??$#', '', $this->getConfig()->getShopHomeURL()).$this->_getToxidLangSeoSnippet();
-	}
+
+    /**
+     * @param null $iLangId
+     * @return string with toxidStartUrl
+     */
+    public function getToxidStartUrl($iLangId = null)
+    {
+        return preg_replace('#index.php\??$#', '', $this->getConfig()->getShopHomeURL()).$this->_getToxidLangSeoSnippet($iLangId);
+    }
 
     /**
      * rewrites given string URL's, which belongs to typo3 and configured in aToxidCurlSource
      * @param string $sContent
-     * @return string with changed URL's 
+     * @param int $iLangId
+     * @param bool $blMultiLang
+     * @return string with changed URL's
      */
-	protected function _rewriteUrls($sContent)
-	{
-		$target      = $this->getConfig()->getConfigParam('sShopURL').$this->_getToxidLangSeoSnippet().'/';
-		$source    = str_replace('.','\.',$this->_getToxidLangSource());
-		$actual    = '%href="'.$source.'(?=.*?.html)%';
-		$should    = 'href="'.$target;
-		return preg_replace($actual, $should, $sContent);
-	}
+    protected function _rewriteUrls($sContent, $iLangId = null, $blMultiLang = false)
+    {
+        
+        if ($blMultiLang == false) {
+            if ($iLangId === null) {
+                $iLangId = oxLang::getInstance()->getBaseLanguage();
+            }
+            $aLanguages = array($iLangId);
+        }
+        else {
+            $aLangs = $this->getConfig()->getConfigParam('aToxidCurlSource');
+            arsort($aLangs);
+            $aLanguages = array_keys($aLangs);
+        }
+
+        foreach ($aLanguages as $iLangId ) {
+            $target    = $this->getConfig()->getConfigParam('sShopURL').$this->_getToxidLangSeoSnippet($iLangId).'/';
+            $source    = str_replace('.','\.',$this->_getToxidLangSource($iLangId));
+            $actual    = '%href="'.$source.'(?=.*?.html)%';
+            $should    = 'href="'.$target;
+            $sContent  = preg_replace($actual, $should, $sContent);
+        }
+
+        return $sContent;
+    }
 
     /**
      * returns string with language specific sourceUrl
+     * @param int $iLangId
      * @param bool $blReset reset object value, and get url again
      * @return string
      */
-	protected function _getToxidLangSource($blReset = false)
-	{
-        if ($this->_sSourceUrlByLang !== null && !$blReset) {
-            return $this->_sSourceUrlByLang;
+    protected function _getToxidLangSource($iLangId = null, $blReset = false)
+    {
+        if ($this->_aSourceUrlByLang === null || $blReset) {
+            $this->_aSourceUrlByLang = $this->getConfig()->getConfigParam('aToxidCurlSource');
         }
-		$langUrls = $this->getConfig()->getConfigParam('aToxidCurlSource');
-		$this->_sSourceUrlByLang = $langUrls[oxLang::getInstance()->getBaseLanguage()];
-		return $this->_sSourceUrlByLang;
-	}
+        if ($iLangId === null) {
+            $iLangId = oxLang::getInstance()->getBaseLanguage();
+        }
+
+        return $this->_aSourceUrlByLang[$iLangId];
+    }
 
     /**
      * returns string with language specific toxidUrlParam
+     * @param int $iLangId
      * @param bool $blReset reset object value, and get url again
      * @return string
      */
-	protected function _getToxidLangUrlParam($blReset = false)
-	{
-        if ($this->_sToxidLangUrlParam !== null && !$blReset) {
-            return $this->_sToxidLangUrlParam;
+    protected function _getToxidLangUrlParam($iLangId = null, $blReset = false)
+    {
+        if ($this->_aToxidLangUrlParam === null || $blReset) {
+            $this->_aToxidLangUrlParam = $this->getConfig()->getConfigParam('aToxidCurlUrlParams');
+        }
+        if ($iLangId === null) {
+            $iLangId = oxLang::getInstance()->getBaseLanguage();
         }
 
-		$langUrlParam = $this->getConfig()->getConfigParam('aToxidCurlUrlParams');
-		$this->_sToxidLangUrlParam = $langUrlParam[oxLang::getInstance()->getBaseLanguage()];
-		return $this->_sToxidLangUrlParam;
-	}
+        return $this->_aToxidLangUrlParam[$iLangId];
+    }
 
     /**
      * returns string with language specific toxidSeoSnippet
+     * @param int $iLangId
      * @param bool $blReset reset object value, and get url again
      * @return string
      */
-	protected function _getToxidLangSeoSnippet($blReset = false)
-	{
-        if ($this->_sRewriteStartUrl !== null && !$blReset) {
-            return $this->_sRewriteStartUrl;
+    protected function _getToxidLangSeoSnippet($iLangId = null, $blReset = false)
+    {
+        if ($this->_aRewriteStartUrl === null || $blReset) {
+            $this->_aRewriteStartUrl = $this->getConfig()->getConfigParam('aToxidCurlSeoSnippets');
         }
-
-		$langSeoSnippets = $this->getConfig()->getConfigParam('aToxidCurlSeoSnippets');
-		$this->_sRewriteStartUrl = $langSeoSnippets[oxLang::getInstance()->getBaseLanguage()];
-		return $this->_sRewriteStartUrl;
-	}
+        if ($iLangId === null) {
+            $iLangId = oxLang::getInstance()->getBaseLanguage();
+        }
+        
+        return $this->_aRewriteStartUrl[$iLangId];
+    }
 
     /**
      * returns typo3 search URL
+     * @param int $iLangId
      * @param bool $blReset reset object value, and get url again
      * @return string
      */
-	protected function _getToxidSearchUrl($blReset = false)
-	{
-        if ($this->_sSearchUrl !== null && !$blReset) {
-            return $this->_sSearchUrl;
+    protected function _getToxidSearchUrl($iLangId = null, $blReset = false)
+    {
+        if ($this->_aSearchUrl === null || $blReset) {
+            $this->_aSearchUrl = $this->getConfig()->getConfigParam('aToxidSearchUrl');
+        }
+        if ($iLangId === null) {
+            $iLangId = oxLang::getInstance()->getBaseLanguage();
         }
 
-		$langSeoSnippets = $this->getConfig()->getConfigParam('aToxidSearchUrl');
-		$this->_sSearchUrl = $langSeoSnippets[oxLang::getInstance()->getBaseLanguage()];
-		return $this->_sSearchUrl;
-	}
+        return $this->_aSearchUrl[$iLangId];
+    }
 
     /**
      * runs search by  given keywords in typo3, and returns results as html
@@ -297,12 +355,12 @@ class toxidCurl extends oxSuperCfg
             $sSearchResult = $this->_rewriteUrls($sSearchResult);
             $this->_aSearchCache[$sKeywords] = $sSearchResult;
         }
-		
+
         if($this->getConfig()->getConfigParam('iUtfMode') !== 1)
-		{
-			return utf8_decode($this->_aSearchCache[$sKeywords]);
-		} else {
-			return $this->_aSearchCache[$sKeywords];
-		}
+        {
+            return utf8_decode($this->_aSearchCache[$sKeywords]);
+        } else {
+            return $this->_aSearchCache[$sKeywords];
+        }
     }
 }
